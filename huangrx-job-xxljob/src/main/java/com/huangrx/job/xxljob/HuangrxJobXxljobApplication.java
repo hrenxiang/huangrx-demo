142804c07edd72f04c0eefe5427f77704f509c8a
hrenxiang <2295701930@qq.com>
2022-04-26 22:04:12 +0800

    新增 huangrx-job-xxljob 模块

---
+++ huangrx-job-xxljob/src/main/java/com/huangrx/job/xxljob/HuangrxJobXxljobApplication.java
@@ -0,0 +1,17 @@
+package com.huangrx.job.xxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author    hrenxiang
+ * @since    2022/4/26 9:44 PM
+ */
+@SpringBootApplication
+public class HuangrxJobXxljobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuangrxJobXxljobApplication.class, args);
+    }
+
+}
---
+++ huangrx-job-xxljob/src/test/java/com/huangrx/jobxxljob/HuangrxJobXxljobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangrx.jobxxljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuangrxJobXxljobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
