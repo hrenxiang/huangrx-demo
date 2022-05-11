e44158ca296c7815fc9d63ef425c3a87ab5c7ab3
hrenxiang <2295701930@qq.com>
2022-05-11 13:45:40 +0800

    为提交的文件

---
+++ huangrx-mvc/src/main/java/com/huangrx/mvc/HuangrxMvcApplication.java
@@ -0,0 +1,13 @@
+package com.huangrx.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuangrxMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuangrxMvcApplication.class, args);
+    }
+
+}
---
+++ huangrx-mvc/src/test/java/com/huangrx/mvc/HuangrxMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangrx.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuangrxMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
