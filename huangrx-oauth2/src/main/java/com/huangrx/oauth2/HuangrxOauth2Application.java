5ebe172e7ed3497ed7325d9cf1ee7716b02c3440
hrenxiang <2295701930@qq.com>
2022-05-11 22:36:38 +0800

    初始化 oauth2 模块

---
+++ huangrx-oauth2/src/main/java/com/huangrx/oauth2/HuangrxOauth2Application.java
@@ -0,0 +1,13 @@
+package com.huangrx.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuangrxOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuangrxOauth2Application.class, args);
+    }
+
+}
---
+++ huangrx-oauth2/src/test/java/com/huangrx/oauth2/HuangrxOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangrx.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuangrxOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
