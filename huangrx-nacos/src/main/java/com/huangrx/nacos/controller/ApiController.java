4357bbe618696a6a9f010cfebf8888ca300ba2f7
hrenxiang <2295701930@qq.com>
2022-07-08 17:07:00 +0800

    update: test @RefreshScope

--- huangrx-nacos/src/main/java/com/huangrx/nacos/controller/ApiController.java
+++ huangrx-nacos/src/main/java/com/huangrx/nacos/controller/ApiController.java
@@ -1,6 +1,8 @@
 package com.huangrx.nacos.controller;
 
 import com.huangrx.nacos.service.ApiServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-04-28 6:48 PM
  */
 @RestController
+// 可以实时刷新nacos配置
+@RefreshScope
 public class ApiController {
+
+    @Value("${huangrx}")
+    private String name;
 
     private final ApiServiceImpl apiService;
 
@@ -21,6 +28,7 @@ public class ApiController {
 
     @GetMapping("/get")
     public String get() {
-        return apiService.hello();
+        //return apiService.hello();
+        return name;
     }
 }
