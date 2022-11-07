31feaf46c350b4dcdb3ec5cfdd544010a458d1a4
hrenxiang <2295701930@qq.com>
2022-11-07 18:11:58 +0800

    add: init

---
+++ huangrx-jvm/src/main/java/com/huangrx/jvm/Math.java
@@ -0,0 +1,8 @@
+package com.huangrx.jvm;
+/**
+ *
+ *
+ * @author hrenxiang
+ * @since 2022-11-07 15:16
+ */public class Math {
+}
---
+++ huangrx-jvm/src/main/java/java/lang/String.java
@@ -0,0 +1,8 @@
+package lang;
+
+/**
+ * @author hrenxiang
+ * @since 2022-11-07 16:59
+ */
+public class String {
+}
